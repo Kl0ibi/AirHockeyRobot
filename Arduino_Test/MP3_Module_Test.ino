1e898bd027ebf4f1c78e2ced1634a31dabc9d1a8
Kl0ibi <87840181+Kl0ibi@users.noreply.github.com>
2021-11-27 17:33:01 +0100

    Add files via upload

---
+++ Arduino_Test/MP3_Module_Test.ino
@@ -0,0 +1,39 @@
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+
+void setup()
+{
+  mySoftwareSerial.begin(9600);
+  Serial.begin(115200);
+  
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); 
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  myDFPlayer.volume(5);  //Set volume value. From 0 to 30
+  myDFPlayer.play(1);  //Play the first mp3
+}
+
+void loop()
+{
+  static unsigned long timer = millis();
+  
+  if (millis() - timer > 3000) {
+    timer = millis();
+ //   myDFPlayer.next();  //Play next mp3  
+  }
+  
+}
